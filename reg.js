f2cfcfcac7b9921407329420009bbe38ff071ab0
Alexskukik <44836756+Alexskukik@users.noreply.github.com>
2019-05-28 12:54:03 +0300

    no message

---
+++ reg.js
@@ -0,0 +1,24 @@
+
+document.getElementById('btnReg').onclick = function(){
+
+
+
+
+
+    fetch('register.html', {
+    method: 'POST',
+    body: JSON.stringify({
+     firstName: document.getElementsByName('firstName'),
+     lastName: document.getElementsByName('lastName'),
+     email: document.getElementsByName('email'),
+     city: document.getElementsByName('city'),
+     login: document.getElementsByName('login'),
+     password: document.getElementsByName('password')
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => console.log(response.status))
+  //.then(json => console.log(json))
+}
