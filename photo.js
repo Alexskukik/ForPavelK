7016ae29024e02800075ca885565121b5d50fe85
Alexskukik <44836756+Alexskukik@users.noreply.github.com>
2019-06-03 14:48:13 +0300

    no message

--- photo.js
+++ photo.js
@@ -6,14 +6,12 @@
 window.onload = function () {
     var a = 0;
     var List = [];
-     loadElem();
+    loadElem();
     getPhoto(a, a + 12);
 
 
 
     function loadPhoto(tmp1, tmp2, tmp3) {
-        console.log(tmp1.url);
-
         var r = document.createElement('div');
         r.className = "photo_row";
         bigPhoto(tmp1, r);
@@ -29,14 +27,8 @@ window.onload = function () {
         var block1 = document.createElement('div');
         block1.className = "photo_block";
 
-        var block2 = document.createElement('div');
-        block1.className = "photo_block";
-
-        var block3 = document.createElement('div');
-        block1.className = "photo_block";
-
         var img1 = document.createElement('img');
-        img1.src = tmp1.url;
+        img1.src = "/image-" + tmp.id + "." + tmp.type;
         img1.onclick = function () {
             console.log('kfk');
             document.getElementById("bigContent").innerHTML = '<img src = "' + img1.src + '">';
@@ -57,51 +49,80 @@ window.onload = function () {
 
     }
 
-    function getPhoto(a, b) {
-        fetch('https://api.myjson.com/bins/f8hqq')
+
+    /*  function loadRev(tmp) {
+         console.log(tmp);
+         var date = new Date(tmp.date);
+        var el = document.getElementById('revList');
+        el.innerHTML += '<div class="rew"> <div class="rew_name">' + tmp.user.firstName + '</div> <div class="rew_text">' + tmp.text + '</div> <div class="rew_date">' + date.toLocaleString() + ' </div></div>';
+ 
+      } */
+
+
+
+    ///получение отзывов с сервера
+    function getPhoto(a) {
+        fetch(`image.get?offset=${a}`)
             .then(response => response.json())
             .then(json => List = json)
             .then(List => {
-                for (var i = a; i < b;) {
-
+                console.log(List);
+                for (var i = a; i < List.length;) {
                     console.log(i);
                     loadPhoto(List[i], List[i + 1], List[i + 2]);
-                    console.log(i);
+                    // console.log(i);
                     i += 3;
                 }
+
             })
+    }
 
-        document.getElementById('next').onclick = function () {
-            a += 12;
 
-            a = check(a);
-            document.getElementById('photoContent').innerHTML = "";
-            getPhoto(a, a + 12);
-            console.log(a, a + 12);
+    /*   function getPhoto(a, b) {
+          fetch('https://api.myjson.com/bins/f8hqq')
+              .then(response => response.json())
+              .then(json => List = json)
+              .then(List => {
+                  for (var i = a; i < b;) {
+  
+                      console.log(i);
+                      loadPhoto(List[i], List[i + 1], List[i + 2]);
+                      console.log(i);
+                      i += 3;
+                  }
+              }) */
+
+    document.getElementById('next').onclick = function () {
+        a += 12;
+
+        a = check(a);
+        document.getElementById('photoContent').innerHTML = "";
+        getPhoto(a, a + 12);
+        console.log(a, a + 12);
+    }
+
+    document.getElementById('prev').onclick = function () {
+        a -= 12;
+
+        a = check(a);
+        document.getElementById('photoContent').innerHTML = "";
+        getPhoto(a, a + 12);
+    }
+
+    function check(a) {
+        if (a < 0) {
+            a = 0;
+        } else if (a + 12 > List.length) {
+            a = List.length - 12;
         }
 
-        document.getElementById('prev').onclick = function () {
-            a -= 12;
-
-            a = check(a);
-            document.getElementById('photoContent').innerHTML = "";
-            getPhoto(a, a + 12);
-        }
-
-        function check(a) {
-            if (a < 0) {
-                a = 0;
-            } else if (a + 12 > List.length) {
-                a = List.length - 12;
-            }
-
-            console.log(a);
-            return a;
-
-        }
-
+        console.log(a);
+        return a;
 
     }
+
+
+
 
 
 
@@ -121,7 +142,7 @@ window.onload = function () {
 
         if (sessionStorage.getItem('status') === "ADMIN")  //авторизован админ
         {
-            
+
         } else if (sessionStorage.getItem('status') === "USER")  //авторизоавн юзер
         {
             // displayBlock("userRewInpt");
